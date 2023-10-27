dac4596ec8d2668dd423e49fb2932006b0dcca22
AbdiJavad <Mirjavadabdi@gmail.com>
2023-10-28 02:20:03 +0330

    first commit

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends GeometricObject {
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle extends GeometricObject {
+
+    public Rectangle(double width, double height) {
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+}
